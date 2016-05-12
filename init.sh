429c2ec06c2bd95236781b70dd6ac47aee2e5dba
eveness <eveness@gmail.com>
2016-05-12 12:48:24 +0300

    gunicorn 1

--- init.sh
+++ init.sh
@@ -1,2 +1,5 @@
-sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo rm /etc/nginx/sites-enabled/default
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo /etc/init.d/gunicorn restart
