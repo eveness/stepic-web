2d7191b7f490cbc999c6ed285271019c1a15bc2f
eveness <eveness@gmail.com>
2016-05-23 00:56:45 +0300

    lesson 2.1, step 11

--- etc/django.py
+++ etc/django.py
@@ -1,5 +1,5 @@
 pythonpath = '/home/box/web/ask/ask'
 bind = '0.0.0.0:8000'
-errorlog = '/home/box/logs/gunicorn-error.log'
-accesslog = '/home/box/logs/gunicorn-access.log'
+errorlog = '/home/box/web/logs/gunicorn-error.log'
+accesslog = '/home/box/web/logs/gunicorn-access.log'
 loglevel = 'debug'
