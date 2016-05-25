5099fc4022bacc9d17b6cf952d811989979ea08a
eveness <eveness@gmail.com>
2016-05-25 11:58:16 +0300

    lesson 2.6, step 11

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 
 
 class QuestionManager(models.Manager):
